6280b0414e31123930c71e53844c7f28072c904e
Lukas Viering <lukas.viering@lukas.vieringC17FV2DPQ6L4>
2021-09-01 13:31:49 +0200

    minor tweaks

--- js/stars.js
+++ js/stars.js
@@ -3,12 +3,16 @@ window.onload = window.onresize = function() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "white";
     for (let i = 0; i < randomRange(1000, 2000); i++) {
         var r = randomRange(1, 3);
-        var x = Math.round(Math.random() * canvas.clientWidth);
-        var y = Math.round(Math.random() * canvas.clientHeight);
+        var x = Math.max(r, Math.round(Math.random() * canvas.clientWidth) - r);
+        var y = Math.max(r, Math.round(Math.random() * canvas.clientHeight) - r);
 
+        var color = "#8ad4ff";
+        if(randomRange(1, 100) > 50){
+            color = "#ffffff";
+        }
+        ctx.fillStyle = color;
         ctx.beginPath();
         ctx.arc(x, y, r, 0, 2 * Math.PI);
         ctx.fill()
